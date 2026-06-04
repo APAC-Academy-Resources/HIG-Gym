7b1608460d58c661de552be8bc3b4ea609e2d2ee
George Ananda <george.ananda@icloud.com>
2026-06-04 10:18:52 +0800

    Initial Commit

---
+++ HIG Camp/HIG_CampApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIG_CampApp.swift
+//  HIG Camp
+//
+//  Created by George Ananda on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct HIG_CampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
