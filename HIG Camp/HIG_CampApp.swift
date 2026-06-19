28afaecce3a874e5de10fbf85af77a0723ea3ac3
George Ananda <george.ananda@icloud.com>
2026-06-19 07:09:30 +0700

    chore(app): document root view selection in app entry

--- HIG Camp/HIG_CampApp.swift
+++ HIG Camp/HIG_CampApp.swift
@@ -1,17 +1,14 @@
-//
-//  HIG_CampApp.swift
-//  HIG Camp
-//
-//  Created by George Ananda on 04/06/26.
-//
-
 import SwiftUI
 
 @main
 struct HIG_CampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // No root or index view
+            // If you need to run it on the simulator,
+            // change the below view to the view you'd like to preview
+            PresentationDetentsDemoView(detents: [.height(200)], interactable: true)
+                .tint(.purple)
         }
     }
 }
