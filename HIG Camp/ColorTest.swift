473042a8d04fca87d1508d32bf9e3beea3cbe54a
George Ananda <george.ananda@icloud.com>
2026-06-19 11:18:54 +0700

    chore: add ColorTest scratch view

--- HIG Camp/ColorTest.swift
+++ HIG Camp/ColorTest.swift
@@ -1,6 +1,6 @@
 //
 //  ColorTest.swift
-//  HIG Camp
+//  HIG Gym
 //
 //  Created by George Ananda on 19/06/26.
 //
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ColorTest: View {
+    let customColor: Color = Color(hue: 0.5, saturation: 1, brightness: 0.5)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Hello Bay")
+                        .foregroundStyle(.primary)
+                    Text("Ini sekunder")
+                        .foregroundStyle(.secondary)
+                    Text("Ini tersier")
+                        .foregroundStyle(.tertiary)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                
+                
+                VStack(alignment: .leading) {
+                    Text("Hello Bay")
+                        .foregroundStyle(.primary)
+                    Text("Ini sekunder")
+                        .foregroundStyle(.secondary)
+                    Text("Ini tersier")
+                        .foregroundStyle(.tertiary)
+                    Text("Ini quarternary")
+                        .foregroundStyle(.quaternary)
+                    Text("Ini quniary")
+                        .foregroundStyle(.quinary)
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+            }
+        }
     }
 }
 
